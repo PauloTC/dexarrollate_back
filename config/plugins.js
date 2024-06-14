7a9dfcc6757e6e701a49e95d2c737bd20e762af4
Paulo Tejada <paulotejada.123a@gmail.com>
2024-06-14 09:55:17 -0500

    update files in local

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,28 @@
-module.exports = () => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
-        params: {
-          Bucket: process.env.AWS_BUCKET_NAME,
+module.exports = () => {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      upload: {
+        config: {
+          provider: "aws-s3",
+          providerOptions: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            region: process.env.AWS_REGION,
+            params: {
+              Bucket: process.env.AWS_BUCKET_NAME,
+            },
+          },
         },
       },
-    },
-  },
-});
+    };
+  } else {
+    return {
+      upload: {
+        config: {
+          provider: "local",
+          providerOptions: {},
+        },
+      },
+    };
+  }
+};
